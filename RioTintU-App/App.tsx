b201593337d369661814e9ba3e679f8c77bc9eb5
andersonalexsandro <anderson.silva.04@outlook.com>
2024-12-30 18:30:46 -0300

    styled and router dependencies

--- RioTintU-App/App.tsx
+++ RioTintU-App/App.tsx
@@ -1,20 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './src/pages/Home';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ RioTintU-App/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { HomeWrapper } from './Home.styles';
+
+export function Home() {
+  return (
+    <HomeWrapper>
+        <View>
+            <Text>Hello World</Text>
+        </View>
+    </HomeWrapper>
+  );
+}
