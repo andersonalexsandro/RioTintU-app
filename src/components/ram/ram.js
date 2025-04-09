a3bec061d08fbfed2ac6c4deb82f9685ceb7b7fb
andersonalexsandro <anderson.silva.04@outlook.com>
2025-04-09 14:40:13 -0300

    refact: small things in ram

--- src/components/ram/ram.js
+++ src/components/ram/ram.js
@@ -2,12 +2,18 @@ import { ram } from '../../index.js';
 
 export function renderRam(ramInstance) {
     const ramGrid = document.getElementById('ram-grid');
+    if (!ramGrid) {
+        console.error('Contêiner RAM não encontrado!');
+        return;
+    }
+
+    ramGrid.innerHTML = '';
 
     for (let i = 0; i < ramInstance.getLengthInBytes(); i++) {
-        const value = ramInstance.get(i);
+        const value = ramInstance.get(i); 
 
         const gridItem = document.createElement('div');
-        gridItem.className = 'ram-item';
+        gridItem.className = 'ram-item'; 
         gridItem.innerHTML = `<span class="ram-address">${i.toString().padStart(3, '0')}</span>: 
                               <span class="ram-value">${value.toString().padStart(3, '0')}</span>`;
 
